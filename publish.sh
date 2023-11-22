2a7a4d02f2b76a617bd1928cdfe43229aaf99cd3
Javier Sanchez <jsanchezm@uco.es>
2023-11-22 09:48:02 +0100

    borrador script local de publicaciones

--- publish.sh
+++
@@ -1,4 +0,0 @@
-rsync -avr --delete pics/ /home/javi/Nextcloud/Desarrollos/javism_website/static/seminars/seminariofate2022/pics/
-cp seminario-fate.html /home/javi/Nextcloud/Desarrollos/javism_website/static/seminars/seminariofate2022/index.html
-cd ~/Nextcloud/Desarrollos/javism_website
-./regenerate.sh
